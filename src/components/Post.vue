8521b504173f2a8f3e3bf591fac0f510c99d39ad
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-04 17:30:45 +0700

    🎉 initial commit

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Post</h2>
+    <h2>{{ todos }}</h2>
+    <h1>{{ message }}</h1>
+    <button v-on:click="reverseMessage">Reverse Message</button>
   </div>
 </template>
 
@@ -10,8 +12,22 @@ export default {
   name: "Post",
   data() {
     return {
-      msg: "Post Post Post"
+      msg: "Post Post Post",
+      todos: [
+        { text: "Learn JavaScript" },
+        { text: "Learn Vue" },
+        { text: "Build something awesome" }
+      ],
+      message: "Hello Vue.js!"
     };
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split("")
+        .reverse()
+        .join("");
+    }
   }
 };
 </script>
