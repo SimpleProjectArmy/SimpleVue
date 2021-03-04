360df1c886ab60f6ae0c5a1823d35d9ea093e0c7
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-04 16:33:33 +0700

    🎉 initial commit

---
+++ src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Post</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  data() {
+    return {
+      msg: "Post Post Post"
+    };
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Post from "@/components/Post";
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/post",
+      name: "post",
+      component: Post
     }
   ]
-})
+});
