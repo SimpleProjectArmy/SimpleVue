1758c205d0979f9a35e0146092c4fcd9a1076bc5
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-05 14:26:04 +0700

    ⚗  Experiment new things todoList

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Post from '@/components/Post'
+import Forms from '@/components/Forms'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/post',
       name: 'post',
       component: Post
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: Forms
     }
   ]
 })
