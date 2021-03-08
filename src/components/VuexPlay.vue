d842c42351e4ad3f33bb05317919596163104043
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-08 16:55:49 +0700

    prepare vunx

---
+++ src/components/VuexPlay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    TEST
+    <Display></Display>
+    <Increment></Increment>
+  </div>
+</template>
+
+<script>
+import Display from './vuex/Display'
+import Increment from './vuex/Increment'
+export default {
+  name: 'VuexPlay',
+  components: {
+    Display: Display,
+    Increment: Increment
+  }
+}
+</script>
